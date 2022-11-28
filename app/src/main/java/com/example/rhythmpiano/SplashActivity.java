cfd55cd9db322449ea2cb8f6191bfb95c841d8da
Hyeran0808 <61002200+Hyeran0808@users.noreply.github.com>
2022-11-28 14:33:41 +0900

    kimegg

---
+++ app/src/main/java/com/example/rhythmpiano/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.rhythmpiano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        moveMain(1);
+    }
+
+    private void moveMain(int sec) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000*sec);
+    }
+}
